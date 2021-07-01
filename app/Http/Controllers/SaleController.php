32ebbead2b6da26ebee2b8103607e9e290f1f093
Igor Xavier <igor.xavier.simoes@gmail.com>
2021-07-01 12:03:16 -0300

    organizar os models

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Sale;
+use App\Models\Sale;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Validator;
 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::any('{all}', function(){
     return view('app');
 })->where(['all' => '.*']);
