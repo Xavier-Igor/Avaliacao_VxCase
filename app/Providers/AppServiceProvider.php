2f917d0e8e8d2f05bcc7e03cfa2daefdb1758a5e
Igor Xavier <igor.xavier.simoes@gmail.com>
2021-07-02 09:43:56 -0300

    implementando docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Jobs\SendNotification;
 use Illuminate\Support\ServiceProvider;
+use App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
+        $this->app->bind('App\Jobs\SendNotification');
+
     }
 
     /**
